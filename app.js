64b3fa5d52f7357056c1737fc01f8a7072847c4b
Mike <mikeytown19@gmail.com>
2016-12-20 05:52:29 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+
+let lastChekced;
+function handleCheck(e){
+    console.log(e);
+    //check if they have the shift key down
+    //and check if they are checking it
+    let inBetween = false;
+    if(e.shiftKey && this.checked){
+        //go ahead and do what we please
+        //loop over every single checked box-shadow
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if(checkbox === this || checkbox === lastChekced){
+            inBetween = !inBetween;
+        }
+
+        if(inBetween){
+            checkbox.checked = true;
+        }
+        })
+    }
+    lastChekced = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
